8478039d0cdb578f47a0e50a2608fa598fd6c5d5
22jy0136 <22jy0136@PC10.jynet2.jec.ac.jp>
2023-09-27 11:39:40 +0900

    HelloWorld12345を3回表示

--- src/javaproject/HelloWorld.java
+++ src/javaproject/HelloWorld.java
@@ -3,7 +3,9 @@ package javaproject;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("HelloWorld12345");
+		for(int i=0; i<3; i++) {
+			System.out.println("HelloWorld12345");
+		}
 
 	}
 
